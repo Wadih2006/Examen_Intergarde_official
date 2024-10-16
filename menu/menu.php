a649259b83457db47fc66fbd3cc97825513f498e
Dania Alfa <207362@vistacollege.nl>
2024-10-16 10:48:29 +0200

    versie 1.0.0

--- menu/menu.php
+++ menu/menu.php
@@ -46,7 +46,7 @@
 <div class="regio-box">
     <div class="regio">
         <a href="#" id="noord-brabant" class="region-link">Noord-Brabant</a>
-        <a href="#" id="noord-limburg" class="region-link">Noord-Limburg</a>
+        <a href="../meldingen/noord-limburg.php" id="noord-limburg" class="region-link">Noord-Limburg</a>
         <a href="../meldingen/zuid-limburg.php" id="zuid-limburg" class="region-link">Zuid-Limburg</a>
     </div>
 </div>
